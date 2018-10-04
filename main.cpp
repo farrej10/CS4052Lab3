f37f9b7d8a2896175db955f3961659dc63a91788
James Farrell <farrej10@ITSERV.SCSS.TCD.IE>
2018-10-04 13:02:22 +0100

    added glm lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 // Macro for indexing vertex buffer
